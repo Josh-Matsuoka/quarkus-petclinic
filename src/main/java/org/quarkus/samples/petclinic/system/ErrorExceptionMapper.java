00dbc6a02f81cff31cd10f54315b7690c80796b2
Joshua Matsuoka <jmatsuok@redhat.com>
2022-06-28 03:50:19 -0400

    Removing exception handler

--- src/main/java/org/quarkus/samples/petclinic/system/ErrorExceptionMapper.java
+++ src/main/java/org/quarkus/samples/petclinic/system/ErrorExceptionMapper.java
@@ -5,7 +5,6 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
-@Provider
 public class ErrorExceptionMapper implements ExceptionMapper<Exception> {
 
     @Inject
